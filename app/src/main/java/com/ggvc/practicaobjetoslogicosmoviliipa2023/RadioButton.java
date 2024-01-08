f0b2be568000b41d9ea85372bbb4726c01d5b63d
LeChustin <jostinaldaz4@gmail.com>
2024-01-08 13:08:27 -0500

    Ejemplo vista radio button

--- app/src/main/java/com/ggvc/practicaobjetoslogicosmoviliipa2023/RadioButton.java
+++ app/src/main/java/com/ggvc/practicaobjetoslogicosmoviliipa2023/RadioButton.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class menu extends AppCompatActivity {
+public class RadioButton extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.menu.menu);
+        setContentView(R.layout.activity_radio_button);
     }
 }
