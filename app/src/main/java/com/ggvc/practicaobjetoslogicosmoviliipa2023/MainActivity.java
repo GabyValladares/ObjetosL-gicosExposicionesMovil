1ef134005ce8f2f7b37c1af6ea69047819cfe64c
JefersonManosalvas <132539368+JefersonManosalvas@users.noreply.github.com>
2024-01-08 12:39:16 -0500

    integracion del proyecto toolbar

--- app/src/main/java/com/ggvc/practicaobjetoslogicosmoviliipa2023/MainActivity.java
+++ app/src/main/java/com/ggvc/practicaobjetoslogicosmoviliipa2023/MainActivity.java
@@ -1,14 +1,26 @@
 package com.ggvc.practicaobjetoslogicosmoviliipa2023;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar1=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar1);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
     }
 }
