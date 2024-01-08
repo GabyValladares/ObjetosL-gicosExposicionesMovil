4a2c7009425d69dc1c201e33f9b904f253ab0c49
saidscv23 <said.carrillo438@ist17dejulio.edu.ec>
2024-01-08 13:06:29 -0500

    toolbar implementacion

---
+++ app/src/main/java/com/ggvc/practicaobjetoslogicosmoviliipa2023/Progressbar.java
@@ -0,0 +1,57 @@
+package com.ggvc.practicaobjetoslogicosmoviliipa2023;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Progressbar extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private Button startButton;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Inicia un hilo para simular un proceso en segundo plano
+                new Thread(new Runnable() {
+                    public void run() {
+
+                        // Lógica para simular el progreso...
+                        while (progressStatus < 100) {
+                            progressStatus += 1;
+
+                            // Actualiza la barra de progreso en el hilo principal
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(progressStatus);
+                                }
+                            });
+
+                            try {
+                                // Agrega una pequeña pausa para simular el progreso
+                                Thread.sleep(50);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/ggvc/practicaobjetoslogicosmoviliipa2023/RadiosActivity.java
@@ -0,0 +1,60 @@
+package com.ggvc.practicaobjetoslogicosmoviliipa2023;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RadiosActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
+    private RadioButton radioButton3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtener referencias de los elementos en el layout
+        radioGroup = findViewById(R.id.radioGroup);
+        radioButton1 = findViewById(R.id.radioButton);
+        radioButton2 = findViewById(R.id.radioButton2);
+        radioButton3 = findViewById(R.id.radioButton3);
+
+        // Configurar el evento clic del botón
+        Button btnMostrar = findViewById(R.id.btnMostrar);
+        btnMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Llamar a la función para mostrar la selección
+                showSelection();
+            }
+        });
+    }
+
+    private void showSelection() {
+        // Obtener el ID de la opción seleccionada
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+
+        // Verificar la opción seleccionada y mostrar un mensaje
+        String message;
+        if (selectedId == radioButton1.getId()) {
+            message = "HAZ ELEGIDO LA OPCION 1";
+        } else if (selectedId == radioButton2.getId()) {
+            message = "HAZ ELEGIDO LA OPCION 2";
+        } else if (selectedId == radioButton3.getId()) {
+            message = "HAZ ELEGIDO LA OPCION 3";
+        } else {
+            message = "NINGUNA OPCION SELECCIONADA";
+        }
+
+        // Mostrar un Toast con el mensaje
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
