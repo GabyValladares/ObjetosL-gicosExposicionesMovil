b4a75f9690282859e0e67c1d0ff79405a0d2ce59
JefersonManosalvas <132539368+JefersonManosalvas@users.noreply.github.com>
2024-01-10 09:23:01 -0500

    repeticion de la clase de toolbar

---
+++ app/src/main/java/com/ggvc/practicaobjetoslogicosmoviliipa2023/Toolbar1.java
@@ -0,0 +1,34 @@
+package com.ggvc.practicaobjetoslogicosmoviliipa2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+
+public class Toolbar1 extends AppCompatActivity {
+
+    private Toolbar tooolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar1);
+        tooolbar=findViewById(R.id.toolbarr);
+        setSupportActionBar(tooolbar);
+
+
+
+    }
+
+    public  boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return  true;
+
+    }
+
+
+}
