49463943df8ab4e17eaa17e8260e505c66d1000e
Henry Paul Diaz Pinchao <henry.diaz054@ist17dejulio.edu.ec>
2024-01-23 16:10:41 -0500

    Creacion del layout para la ficha vehicular

--- app/src/main/java/com/ggvc/practicaobjetoslogicosmoviliipa2023/MainActivity.java
+++ app/src/main/java/com/ggvc/practicaobjetoslogicosmoviliipa2023/MainActivity.java
@@ -1,14 +1,33 @@
 package com.ggvc.practicaobjetoslogicosmoviliipa2023;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    EditText edNombre;
+    Button btnProcesar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edNombre = findViewById(R.id.txtNombre);
+        btnProcesar = findViewById(R.id.btnProcesar);
+
+    }
+
+    public void Procesar() {
+        Bundle datos = new Bundle();
+
+        datos.putString("nombre", edNombre.getText().toString());
+
+        Intent i = new Intent(this, ResultadoFV.class);
     }
 }
+
---
+++ app/src/main/java/com/ggvc/practicaobjetoslogicosmoviliipa2023/ResultadoFV.java
@@ -0,0 +1,15 @@
+package com.ggvc.practicaobjetoslogicosmoviliipa2023;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultadoFV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_fv);
+
+    }
+}
